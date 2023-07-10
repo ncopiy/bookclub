b56ea30fa8d175cafc65688fd4212a3036af6a3d
ncopiy <ncopiy@yandex.com>
2023-07-10 23:41:11 +0300

    Add query params support

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const DB_PREFIX = document.URL.includes("localhost") ? "db/test/" : "db/"
 const CURRENT_DB = DB_PREFIX + "current.json";
 const NAVIGATION_DB = DB_PREFIX + "navigation.json";
 
+const PAGE_QUERY_PARAM = "page";
+
+var SECTION_TAG_TO_DATA_SOURCE = {};
+
 function formatEndOfStrings(text) {
     return text.replaceAll('\n', '<br>');
 }
@@ -37,14 +41,32 @@ function getFormattedPosts(posts) {
     return text;
 }
 
-function getNavigationLink(name, source) {
+function getNavigationLink(name, source, tag) {
     return [
         `<div onclick='fillPosts("`,
         source,
+        `", "`,
+        tag,
         `");'>`,
         name,
         `</div>`,
     ].join('');
+}
+
+function fillTags(navigation) {
+    for (let i = 0; i < navigation.length; i++) {
+        if (navigation[i].tag) {
+            SECTION_TAG_TO_DATA_SOURCE[navigation[i].tag] = DB_PREFIX + navigation[i].source
+        }
+
+        if (navigation[i].inner.length > 0) {
+            for (let j = 0; j < navigation[i].inner.length; j++) {
+                if (navigation[i].inner[j].tag) {
+                    SECTION_TAG_TO_DATA_SOURCE[navigation[i].inner[j].tag] = DB_PREFIX + navigation[i].inner[j].source
+                }
+            }
+        }
+    }
 }
 
 function getFormattedNavigation(navigation) {
@@ -53,7 +75,11 @@ function getFormattedNavigation(navigation) {
     for (let i = 0; i < navigation.length; i++) {
         text += "<li>";
         if (navigation[i].source.length > 0) {
-            text += getNavigationLink(navigation[i].name, DB_PREFIX + navigation[i].source);
+            text += getNavigationLink(
+                navigation[i].name,
+                DB_PREFIX + navigation[i].source,
+                navigation[i].tag
+            );
         } else {
             text += navigation[i].name;
         }
@@ -62,7 +88,11 @@ function getFormattedNavigation(navigation) {
             text += "<ul>";
             for (let j = 0; j < navigation[i].inner.length; j++) {
                 text += "<li>";
-                text += getNavigationLink(navigation[i].inner[j].name, DB_PREFIX + navigation[i].inner[j].source);
+                text += getNavigationLink(
+                    navigation[i].inner[j].name,
+                    DB_PREFIX + navigation[i].inner[j].source,
+                    navigation[i].inner[j].tag
+                );
                 text += "</li>";
             }
             text += "</ul>";
@@ -94,21 +124,47 @@ async function fillNavigation() {
         return
     }
 
+    fillTags(navigation.navigation)
+
     $(".top-menu").html(getFormattedNavigation(navigation.navigation));
 }
 
-async function fillPosts(source) {
+async function fillPosts(source, tag) {
     posts = await fetchData(source);
     if (posts == null) {
         return
     }
 
     $(".posts").html(getFormattedPosts(posts.posts));
+
+    if (tag) {
+        var queryParams = new URLSearchParams();
+        queryParams.set(PAGE_QUERY_PARAM, tag);
+        history.replaceState(null, null, "?" + queryParams.toString());
+    } else {
+        history.replaceState(null, null, "?");
+    }
+}
+
+function getSectionTag() {
+    if (SECTION_TAG_TO_DATA_SOURCE.length == 0) {
+        return null;
+    }
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const pageTag = urlParams.get(PAGE_QUERY_PARAM);
+
+    if (pageTag in SECTION_TAG_TO_DATA_SOURCE) {
+        return pageTag;
+    }
+
+    return null;
 }
 
 async function fillMainData() {
     await fillNavigation();
-    await fillPosts(CURRENT_DB);
+    var currentSectionTag = getSectionTag()
+    await fillPosts(currentSectionTag == null ? CURRENT_DB : SECTION_TAG_TO_DATA_SOURCE[currentSectionTag], currentSectionTag);
 }
 
 fillMainData();
